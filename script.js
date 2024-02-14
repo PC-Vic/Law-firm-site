f326a9e14fefc36c0a296d8a785545dd20cf752e
Victor Nieves <c_cc_vic@yahoo.com>
2024-02-13 22:33:17 -0500

    added event listener to add box shadow to header on scroll

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// JavaScript code to change header color on scroll
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('header');
+    var scrollPosition = window.scrollY;
+
+    // Add 'scrolled' class to header when scrolled down
+    if (scrollPosition > 0) {
+        header.classList.add('scrolled');
+    } else {
+        // Remove 'scrolled' class when back to the top
+        header.classList.remove('scrolled');
+    }
+});
